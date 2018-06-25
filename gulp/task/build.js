ac0c4443fa45130eb3b442c4683f7a8a5a320550
Brayn <ilogirb23@yahoo.com>
2018-06-25 19:09:44 +0800

    completed gulp buil task

--- gulp/task/build.js
+++ gulp/task/build.js
@@ -13,27 +13,30 @@ gulp.task('previewDist', function() {
 	browserSync.init({
 		notify: false, //removes notification uppon changes
 		server: {
-			baseDir: "dist"//points to the folder the browser will open
+			baseDir: "docs"//points to the folder the browser will open
 		}
 	});
 
 })
 
-gulp.task('deleteDistFolder', function() {
-	return del("./dist");
+gulp.task('deleteDistFolder',['icons'], function() {
+	return del("./docs");
 })
 
-gulp.task('optimizeImages',['deleteDistFolder', 'icons'], function() {
+gulp.task('optimizeImages',['deleteDistFolder'], function() {
 	return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
 	.pipe(imagemin({
 		progressive: true,
 		interlaced: true,
 		multipass: true
 	}))
-	.pipe(gulp.dest("./dist/assets/images"));
+	.pipe(gulp.dest("./docs/assets/images"));
 });
+gulp.task('useminTrigger', ['deleteDistFolder'], function() {
+	gulp.start("usemin")
+})
 
-gulp.task('usemin',['deleteDistFolder', 'styles', 'scripts'], function() {
+gulp.task('usemin',['styles', 'scripts'], function() {
 	return gulp.src("./app/index.html")
 	.pipe(usemin({
 		css: [ rev(), cssnano() ],
@@ -42,7 +45,7 @@ gulp.task('usemin',['deleteDistFolder', 'styles', 'scripts'], function() {
 		inlinejs: [ uglify() ],
 		inlinecss: [ cleanCss(), 'concat' ]
 	}))
-	.pipe(gulp.dest("./dist"));
+	.pipe(gulp.dest("./docs"));
 })
 
-gulp.task('build',['deleteDistFolder','optimizeImages','usemin']);
+gulp.task('build',['deleteDistFolder','optimizeImages','useminTrigger']);
